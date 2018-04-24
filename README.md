17b03391b054d6a1832a2046a045f783e6e08e9a
wiyr <nightwiyr@gmail.com>
2018-04-24 14:37:11 +0800

    fix install

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 ### 安装
-    wget https://raw.github.com/wiyr/vim/master/install.sh -O - | sh
+    wget https://raw.github.com/wiyr/vim/master/install.sh -O - | bash
--- install.sh
+++ install.sh
@@ -3,6 +3,10 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+if [ -e $HOME/.vimrc ]; then
+    mv $HOME/.vimrc $HOME/.vimrc.bak
+fi
+
 if [ -d $HOME/.vim ]; then
     mv $HOME/.vim $HOME/.vim.bak
 fi
@@ -13,5 +17,5 @@ if [ ! -e $HOME/.vim/vimrc ]; then
 fi
 ln -s $HOME/.vim/vimrc $HOME/.vimrc
 
-/usr/bin/env git clone https://github.com/gmarik/vundle.git $HOME/.vim/bundle/vundle
+/usr/bin/env git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
 vim +BundleInstall +qall
