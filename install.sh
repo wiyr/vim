84324d63dd721162ff56e6b81cc38288a0556cf5
wiyr <nightwiyr@gmail.com>
2018-07-02 17:29:05 +0800

    fix chaos output when install
    remove warning of "vim : Warnning : Input is not from a terminal"

--- install.sh
+++ install.sh
@@ -18,4 +18,4 @@ fi
 ln -s $HOME/.vim/vimrc $HOME/.vimrc
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-vim +PlugInstall +qall
+vim +PlugInstall +qall < /dev/tty
